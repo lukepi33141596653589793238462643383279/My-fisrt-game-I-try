65805a5559b2acb0f1c27f4870a6037f659690d2
luke_314159265 <lucasdeoliveiratuci19@gmail.com>
2026-05-22 09:40:15 -0300

    Implement circle collision detection function

---
+++ collision.js
@@ -0,0 +1,9 @@
+function circleCollision(circle1, circle2) {
+
+    const dx = circle1.x - circle2.x
+    const dy = circle1.y - circle2.y
+
+    const distance = Math.sqrt(dx * dx + dy * dy)
+
+    return distance < circle1.radius + circle2.radius
+}

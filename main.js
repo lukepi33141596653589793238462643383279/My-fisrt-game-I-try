82eeed7c71d15867ba886acbc42d4912866fa995
luke_314159265 <lucasdeoliveiratuci19@gmail.com>
2026-05-22 09:46:27 -0300

    Add player movement and rendering logic

--- main.js
+++ main.js
@@ -4,19 +4,55 @@ const ctx = canvas.getContext("2d")
 canvas.width = 800
 canvas.height = 600
 
+const player = new Player(
+    100,
+    100,
+    15,
+    "yellow"
+)
+
 function update() {
 
+    player.velocity.x = 0
+    player.velocity.y = 0
+
+    if (keys.w) {
+        player.velocity.y = -player.speed
+    }
+
+    if (keys.s) {
+        player.velocity.y = player.speed
+    }
+
+    if (keys.a) {
+        player.velocity.x = -player.speed
+    }
+
+    if (keys.d) {
+        player.velocity.x = player.speed
+    }
+
+    player.update()
 }
 
 function render() {
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    )
 
+    player.draw(ctx)
+
+    drawScore(ctx)
 }
 
 function gameLoop() {
 
     update()
+
     render()
 
     requestAnimationFrame(gameLoop)
